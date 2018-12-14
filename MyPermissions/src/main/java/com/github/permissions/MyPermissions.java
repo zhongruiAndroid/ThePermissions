a2e9486e410f9b3b326300372ebdc820951a3442
r-zhong <r-zhong@logictech.cn>
2018-12-14 17:06:28 +0800

    file

---
+++ MyPermissions/src/main/java/com/github/permissions/MyPermissions.java
@@ -0,0 +1,9 @@
+package com.github.permissions;
+
+/**
+ * @createBy Administrator
+ * @time 2018-12-14 15:16
+ */
+public class MyPermissions {
+
+}
---
+++ app/src/main/java/com/test/permissions/MainActivity.java
@@ -0,0 +1,30 @@
+package com.test.permissions;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    }
+
+
+}
